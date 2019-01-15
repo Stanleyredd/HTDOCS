c602cc47ef049c7fa902d0339d034d0a692abf73
stanley <stanley2001@live.nl>
2019-01-15 09:06:39 +0100

    alle opdrachten PHP

---
+++ h01/page.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stanley
+ * Date: 8-1-2019
+ * Time: 09:39
+ */
+echo 'yoyoyoyo';
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stanley
+ * Date: 8-1-2019
+ * Time: 09:36
+ */
+echo 'test';
+
+<a href="../index.php">opdracht1</a>
